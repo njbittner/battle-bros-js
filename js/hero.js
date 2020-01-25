5d12bb753edd00c1e98dde518250922fe4156736
Nathan Bittner <njbittner@gmail.com>
2020-01-24 17:45:52 -0800

    refactor into separate files

---
+++ js/hero.js
@@ -0,0 +1,67 @@
+
+// Hero constructor, overriding the sprite interace.
+function Hero(game, x, y){
+    // Phasor.Sprite
+    Phaser.Sprite.call(this, game, x, y, 'hero');
+    this.game.physics.enable(this);
+    this.anchor.set(0.5, 0.5);
+    this.body.collideWorldBounds=true;
+
+    this.animations.add('stop', [0]);
+    this.animations.add('run', [1,2], 8, true);
+    this.animations.add('jump', [3]);
+    this.animations.add('fall', [4]);
+
+};
+
+Hero.prototype = Object.create(Phaser.Sprite.prototype);
+Hero.prototype.constructor = Hero;
+Hero.prototype.move = function(direction){
+    const SPEED = 200;
+    // this.x += direction*2.5; // per-frame in pixels.
+    this.body.velocity.x = direction*SPEED; // for physics engine.
+    if (this.body.velocity.x < 0) {
+        this.scale.x = -1;
+    }
+    else if (this.body.velocity.x > 0){
+        this.scale.x = 1;
+    }
+};
+
+Hero.prototype.jump = function(){
+    const JUMP_SPEED = 600;
+    // Very cool method to check if bottom of sprite
+    // is in contact with anything
+    let canJump = this.body.touching.down;
+
+    if (canJump){
+        this.body.velocity.y = -JUMP_SPEED;
+    }
+    return canJump;
+};
+
+Hero.prototype._getAnimationName = function() {
+    let name = 'stop'; // default
+    if (this.body.velocity.y < 0){
+        name = 'jump';
+    }
+    else if (this.body.velocity.y >= 0 && !this.body.touching.down){
+        name = 'fall';
+    }
+    else if (this.body.velocity.x != 0 && this.body.touching.down){
+        name = 'run'
+    }
+    return name;
+}
+
+Hero.prototype.bounce = function(){
+    const BOUNCE_SPEED = 200;
+    this.body.velocity.y = -BOUNCE_SPEED;
+};
+
+Hero.prototype.update = function(){
+    let animationName = this._getAnimationName();
+    if (this.animations.name !== animationName){
+        this.animations.play(animationName);
+    }
+};
---
+++ js/spider.js
@@ -0,0 +1,37 @@
+
+function Spider(game, x,y){
+    // call parent init
+    Phaser.Sprite.call(this, game, x, y, 'spider');
+
+    // anchor
+    this.anchor.set(0.5); // what dim is this?
+    //animation
+    this.animations.add('crawl', [0, 1, 2], 8, true);
+    this.animations.add('die', [0,4,0,4,0,4,3,3,3,3,3,3], 12, false);
+    this.animations.play('crawl');
+
+    // physics
+    this.game.physics.enable(this);
+    this.body.collideWorldBounds = true;
+    this.body.velocity.x = Spider.SPEED;
+}
+Spider.SPEED = 100;
+Spider.prototype = Object.create(Phaser.Sprite.prototype);
+Spider.prototype.constructor = Spider;
+
+Spider.prototype.update = function() {
+    // check walls -- touching for other spites, blocked for world bounds?
+    if (this.body.touching.right || this.body.blocked.right){
+        this.body.velocity.x = -Spider.SPEED;
+    }
+    else if (this.body.touching.left || this.body.blocked.left){
+        this.body.velocity.x = Spider.SPEED;
+    }
+};
+
+Spider.prototype.die = function(){
+    this.body.enable = false;
+    this.animations.play('die').onComplete.addOnce(function() {
+        this.kill();
+    }, this);
+};
