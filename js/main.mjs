074149934cd67dc04cba347773c855a156f69891
Nathan Bittner <njbittner@gmail.com>
2020-01-27 12:00:35 -0800

    Draw Background, lose sprite

--- js/main.mjs
+++ js/main.mjs
@@ -2,9 +2,7 @@ import {PlayState} from './playstate.mjs';
 
 class Game extends Phaser.Game{
     constructor() {
-        super(960,
-            600, 
-            Phaser.AUTO, 'content', null);
+        super(960, 600, Phaser.AUTO, 'content', null);
         this.state.add('play', PlayState);
         // name, cache, world objects, params
         this.state.start('play', true, false, {level:0});
